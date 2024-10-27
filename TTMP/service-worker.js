3b50331a7090a7018eca0f40db2ac281f9748350
Alan Hexfury <firedragon8492@gmail.com>
2024-10-27 01:14:04 -0400

    Updated service worker for TTMP

--- TTMP/service-worker.js
+++ TTMP/service-worker.js
@@ -1,46 +1,48 @@
-const CACHE_NAME = 'tome-tales-cache-v1-1';
+const CACHE_NAME = "tome-tales-cache-v1-1";
 const urlsToCache = [
-  '/',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/lua/main.lua'
+  "/",
+  "/index.js",
+  "/src/output.css",
+  "/app.js",
+  "/manifest.json",
+  "/package.json",
+  "/favicon.ico",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/lua/main.lua",
+  "/fengari-web.js",
 ];
 
 // Install the service worker and cache all necessary assets
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // Fetch assets from the cache when offline
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return the response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      // Cache hit - return the response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
   );
 });
 
 // Update the service worker and clear old caches
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => 
+    caches.keys().then((cacheNames) =>
       Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
