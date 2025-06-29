4a99cb5f586f352f4cc312a9a7306ec1a0923e93
Alan Hexfury <firedragon8492@gmail.com>
2025-06-29 18:38:57 -0400

    Forgot the CSS.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./assets/htmlAssets/**/*.html", "./index.html"], // Scan all HTML and JS files for Tailwind classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
